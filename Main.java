b2e5c4d08fd3df8f081f430a41517438a24728b9
Jean Rehr <49696706+JeanRehr@users.noreply.github.com>
2024-09-16 23:00:01 -0300

    Update Main.java

--- Main.java
+++ Main.java
@@ -3,6 +3,7 @@ public class Main {
         AVLtree tree = new AVLtree();
         Text text = new Text();
         int j = 10;
+        boolean printBeforeAfter = false;
 
         int userInt;
         short userOpt = 100;
@@ -10,13 +11,13 @@ public class Main {
 
 
         while (userOpt != 9) {
-			text.options();
+            text.options();
 
-            userOpt = text.getUserOption((short) 12, (short) 0);
+            userOpt = text.getUserOption((short) 15, (short) 0);
 
             switch (userOpt) {
             case 1: // Print Tree
-                tree.printTree();
+                tree.printTree(tree.root);
                 break;
             case 2: // Search
                 System.out.print("Search which number? ");
@@ -25,21 +26,25 @@ public class Main {
                 break;
             case 3: // Insert
                 System.out.print("Insert which number? ");
-			    userInt = text.getInt();
-                System.out.println("Tree before operation:");
-                tree.printTree();
+                userInt = text.getInt();
+                if (printBeforeAfter) {
+                    System.out.println("Tree before operation:");
+                    tree.printTree(tree.root);
+                }
                 tree.insert(userInt);
                 System.out.println("Tree after operation:");
-                tree.printTree();
+                tree.printTree(tree.root);
                 break;
             case 4: // Remove
                 System.out.print("Remove which number? ");
-			    userInt = text.getInt();
-                System.out.println("Tree before operation:");
-                tree.printTree();
+                userInt = text.getInt();
+                if (printBeforeAfter) {
+                    System.out.println("Tree before operation:");
+                    tree.printTree(tree.root);
+                }
                 tree.remove(userInt);
                 System.out.println("Tree after operation:");
-                tree.printTree();
+                tree.printTree(tree.root);
                 break;
             case 5: // Preorder
                 tree.preorder();
@@ -69,7 +74,7 @@ public class Main {
                 break;
             case 11:
                 System.out.print("Operations on which node? ");
-			    userInt = text.getInt();
+                userInt = text.getInt();
                 if (tree.searchNode(userInt) != null) {
                     System.out.println(
                         "Minimum of sub-tree: " +
@@ -81,7 +86,9 @@ public class Main {
                         "\nSuccessor of sub-tree: " +
                         tree.getSuccessorData(tree.searchNode(userInt)) +
                         "\nPredecessor of sub-tree: " +
-                        tree.getPredecessorData(tree.searchNode(userInt))
+                        tree.getPredecessorData(tree.searchNode(userInt)) +
+                        "\nRoot of the entire tree: " +
+                        tree.getRootData()
                     );
                 } else { 
                     System.out.println("Data/sub-tree not found.");
@@ -89,20 +96,57 @@ public class Main {
                 break;
             case 12:
                 System.out.print("Size of the array? ");
-			    userInt = text.getInt();
+                userInt = text.getInt();
                 System.out.println("Inserting " + userInt + " numbers in intervals of 10.");
                 System.out.println("Creating numbers.");
                 int numbersForInserting[] = new int[userInt];
+                if (j != 10) {
+                    System.out.print("Press 1 to start assignment from 10. ");
+                    int reset = text.getInt();
+                    if (reset == 1) {
+                        j = 10;
+                    }
+                }
                 for (int i = 0; i < numbersForInserting.length; i++) {
                     if (j % 10 == 0) {
                         numbersForInserting[i] = j;
                         j += 10;
                     }
                 }
+                if (printBeforeAfter) {
+                    System.out.println("Tree before operation:");
+                    tree.printTree(tree.root);
+                }
                 System.out.println("Inserting into the tree.");
                 for (int i = 0; i < numbersForInserting.length; i++) {
                     tree.insert(numbersForInserting[i]);
                 }
+                System.out.println("Tree after operation:");
+                tree.printTree(tree.root);
+                break;
+            case 13:
+                System.out.print("Delete which sub-tree? ");
+                userInt = text.getInt();
+                if (printBeforeAfter) {
+                    System.out.println("Tree before operation:");
+                    tree.printTree(tree.root);
+                }
+                tree.massRemove(userInt);
+                System.out.println("Tree after operation:");
+                tree.printTree(tree.root);
+                break;
+            case 14:
+                System.out.print("Print from which sub-tree? ");
+                userInt = text.getInt();
+                if (tree.searchNode(userInt) != null) {
+                    tree.printTree(tree.searchNode(userInt));
+                } else {
+                    System.out.println("Data/sub-tree not found.");
+                }
+                break;
+            case 15:
+                printBeforeAfter = !printBeforeAfter;
+                System.out.println("Print before and after operations: " + printBeforeAfter);
                 break;
             }
         }
